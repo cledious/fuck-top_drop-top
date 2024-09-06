acc21f013c97f9fb60f0133ff9144027b0110422
cledious <89284490+cledious@users.noreply.github.com>
2024-09-05 18:30:58 -0600

    khfkghjgsd

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+function message(){
+    var Name = document.getElementById('name');
+    var email = document.getElementById('email');
+    var msg = document.getElementById('msg');
+    const success = document.getElementById('success');
+    const danger = document.getElementById('danger');
+
+    if(Name.value === '' || email.value === '' || msg.value === ''){
+        danger.style.display = 'block';
+    }
+    else{
+        setTimeout(() => {
+            Name.value = '';
+            email.value = '';
+            msg.value = '';
+        }, 2000);
+
+        success.style.display = 'block';
+    }
+
+
+    setTimeout(() => {
+        danger.style.display = 'none';
+        success.style.display = 'none';
+    }, 4000);
+}
+function show() {
+    const container = document.getElementsByClassName('header-contact_form');
+    const trigger = document.getElementById('contacttrigger');
+}
